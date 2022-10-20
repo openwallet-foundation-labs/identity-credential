02fc6e85e8b2c9f46f96acbb53892d13595ffd1a
Jov Mit <mitrejcevski@gmail.com>
2022-10-20 17:29:30 +0200

    Use the UserAuthPromptBuilder in the TrasnferDocumentFragment

--- appholder/src/main/java/com/android/mdl/app/fragment/TransferDocumentFragment.kt
+++ appholder/src/main/java/com/android/mdl/app/fragment/TransferDocumentFragment.kt
@@ -9,24 +9,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
-import androidx.biometric.BiometricManager
-import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
-import androidx.biometric.BiometricManager.BIOMETRIC_SUCCESS
-import androidx.biometric.BiometricPrompt
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.android.identity.CredentialDataResult
 import com.android.mdl.app.R
+import com.android.mdl.app.authprompt.UserAuthPromptBuilder
 import com.android.mdl.app.databinding.FragmentTransferDocumentBinding
 import com.android.mdl.app.document.Document
 import com.android.mdl.app.document.KeysAndCertificates
+import com.android.mdl.app.documentdata.DocumentDataReader
 import com.android.mdl.app.readerauth.SimpleReaderTrustStore
 import com.android.mdl.app.transfer.TransferManager
 import com.android.mdl.app.util.TransferStatus
 import com.android.mdl.app.viewmodel.TransferDocumentViewModel
 import org.jetbrains.anko.support.v4.runOnUiThread
-import java.util.concurrent.Executor
-
 
 class TransferDocumentFragment : Fragment() {
 
@@ -35,9 +32,9 @@ class TransferDocumentFragment : Fragment() {
     }
 
     private var _binding: FragmentTransferDocumentBinding? = null
-    private lateinit var vm: TransferDocumentViewModel
     private var readerCommonName = ""
     private var readerIsTrusted: Boolean = false
+    private val viewModel: TransferDocumentViewModel by viewModels()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -48,108 +45,97 @@ class TransferDocumentFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentTransferDocumentBinding.inflate(inflater)
-        vm = ViewModelProvider(this).get(TransferDocumentViewModel::class.java)
-
         binding.fragment = this
-        binding.vm = vm
-
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
-
+        binding.vm = viewModel
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            onBackPressedCallback
+        )
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        vm.getTransferStatus().observe(viewLifecycleOwner) { transferStatus ->
+        viewModel.getTransferStatus().observe(viewLifecycleOwner) { transferStatus ->
             when (transferStatus) {
-                TransferStatus.QR_ENGAGEMENT_READY -> {
-                    Log.d(LOG_TAG, "Engagement Ready")
-                }
-
-                TransferStatus.CONNECTED -> {
-                    Log.d(LOG_TAG, "Connected")
-                }
-
-                TransferStatus.REQUEST -> {
-                    Log.d(LOG_TAG, "Request")
-                    // TODO: Add option to the user select the which document share when there
-                    //  are more than one for now we are just returning the first document we found
-                    try {
-                        val trustStore = SimpleReaderTrustStore(
-                            KeysAndCertificates.getTrustedReaderCertificates(requireContext())
-                        )
-                        val requestedDocuments = vm.getRequestedDocuments()
-                        requestedDocuments.forEach { reqDoc ->
-                            val doc = vm.getDocuments().find { reqDoc.docType == it.docType }
-                            if (reqDoc.readerAuth != null && reqDoc.readerAuthenticated) {
-                                val readerChain = reqDoc.readerCertificateChain
-                                if (readerChain != null) {
-                                    val trustPath =
-                                        trustStore.createCertificationTrustPath(readerChain.toList())
-
-                                    // Look for the common name in the root certificate if it is trusted or not
-                                    val certChain = if (trustPath?.isNotEmpty() == true) {
-                                        trustPath
-                                    } else {
-                                        readerChain
-                                    }
-
-                                    certChain.first().subjectX500Principal.name.split(",")
-                                        .forEach { line ->
-                                            val (key, value) = line.split("=", limit = 2)
-                                            if (key == "CN") {
-                                                readerCommonName = value
-                                            }
-                                        }
-
-                                    // Add some information about the reader certificate used
-                                    if (trustStore.validateCertificationTrustPath(trustPath)) {
-                                        readerIsTrusted = true
-                                        binding.txtDocuments.append("- Trusted reader auth used: ($readerCommonName)\n")
-                                    } else {
-                                        readerIsTrusted = false
-                                        binding.txtDocuments.append("- Not trusted reader auth used: ($readerCommonName)\n")
-                                    }
-                                }
-                            }
-                            if (doc != null) {
-                                binding.txtDocuments.append("- ${doc?.userVisibleName} (${doc?.docType})\n")
-                            } else {
-                                binding.txtDocuments.append("- No document found for ${reqDoc.docType}\n")
-                            }
-                        }
-
-                        // TODO Request the data, iterate over it and see which properties need signature
-                        // Then ask for user consent and send response
-                        sendResponseWithConsent(false)
-                    } catch (e: Exception) {
-                        val message = "On request received error: ${e.message}"
-                        Log.e(LOG_TAG, message, e)
-                        Toast.makeText(
-                            requireContext(), message,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        binding.txtConnectionStatus.append("\n$message")
-                    }
-                }
-
-                TransferStatus.DISCONNECTED -> {
-                    Log.d(LOG_TAG, "Disconnected")
-                    hideButtons()
-                    TransferManager.getInstance(requireContext()).disconnect()
-                }
-
-                TransferStatus.ERROR -> {
-                    Toast.makeText(
-                        requireContext(), "An error occurred.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    hideButtons()
-                    TransferManager.getInstance(requireContext()).disconnect()
-                }
+                TransferStatus.QR_ENGAGEMENT_READY -> Log.d(LOG_TAG, "Engagement Ready")
+                TransferStatus.CONNECTED -> Log.d(LOG_TAG, "Connected")
+                TransferStatus.REQUEST -> onTransferRequested()
+                TransferStatus.DISCONNECTED -> onTransferDisconnected()
+                TransferStatus.ERROR -> onTransferError()
             }
         }
+    }
+
+    private fun onTransferRequested() {
+        Log.d(LOG_TAG, "Request")
+        // TODO: Add option to the user select the which document share when there
+        //  are more than one for now we are just returning the first document we found
+        try {
+            val trustStore = SimpleReaderTrustStore(
+                KeysAndCertificates.getTrustedReaderCertificates(requireContext())
+            )
+            val requestedDocuments = viewModel.getRequestedDocuments()
+            requestedDocuments.forEach { reqDoc ->
+                val doc = viewModel.getDocuments().find { reqDoc.docType == it.docType }
+                if (reqDoc.readerAuth != null && reqDoc.readerAuthenticated) {
+                    val readerChain = reqDoc.readerCertificateChain
+                    if (readerChain != null) {
+                        val trustPath = trustStore.createCertificationTrustPath(readerChain.toList())
+                        // Look for the common name in the root certificate if it is trusted or not
+                        val certChain = if (trustPath?.isNotEmpty() == true) {
+                            trustPath
+                        } else {
+                            readerChain
+                        }
+
+                        certChain.first().subjectX500Principal.name.split(",")
+                            .forEach { line ->
+                                val (key, value) = line.split("=", limit = 2)
+                                if (key == "CN") {
+                                    readerCommonName = value
+                                }
+                            }
+
+                        // Add some information about the reader certificate used
+                        if (trustStore.validateCertificationTrustPath(trustPath)) {
+                            readerIsTrusted = true
+                            binding.txtDocuments.append("- Trusted reader auth used: ($readerCommonName)\n")
+                        } else {
+                            readerIsTrusted = false
+                            binding.txtDocuments.append("- Not trusted reader auth used: ($readerCommonName)\n")
+                        }
+                    }
+                }
+                if (doc != null) {
+                    binding.txtDocuments.append("- ${doc.userVisibleName} (${doc.docType})\n")
+                } else {
+                    binding.txtDocuments.append("- No document found for ${reqDoc.docType}\n")
+                }
+            }
+
+            // TODO Request the data, iterate over it and see which properties need signature
+            // Then ask for user consent and send response
+
+            requestUserAuth(false)
+        } catch (e: Exception) {
+            val message = "On request received error: ${e.message}"
+            Log.e(LOG_TAG, message, e)
+            Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+            binding.txtConnectionStatus.append("\n$message")
+        }
+    }
+
+    private fun onTransferDisconnected() {
+        Log.d(LOG_TAG, "Disconnected")
+        hideButtons()
+        TransferManager.getInstance(requireContext()).disconnect()
+    }
+
+    private fun onTransferError() {
+        Toast.makeText(requireContext(), "An error occurred.", Toast.LENGTH_SHORT).show()
+        hideButtons()
+        TransferManager.getInstance(requireContext()).disconnect()
     }
 
     private fun hideButtons() {
@@ -160,46 +146,25 @@ class TransferDocumentFragment : Fragment() {
         binding.btOk.visibility = View.VISIBLE
     }
 
-    private val executor = Executor {
-        if (Looper.myLooper() == null) {
-            Looper.prepare()
-        }
-
-        it.run()
+    private fun requestUserAuth(forceLskf: Boolean) {
+        val userAuthRequest = UserAuthPromptBuilder.requestUserAuth(this)
+            .withTitle(getString(R.string.bio_auth_title))
+            .withSubtitle(getSubtitle())
+            .withDescription(formatEntryNames(viewModel.getEntryNames()))
+            .withNegativeButton(getString(R.string.bio_auth_use_pin))
+            .withSuccessCallback { authenticationSucceeded() }
+            .withCancelledCallback { retryForcingPinUse() }
+            .withFailureCallback { authenticationFailed() }
+            .setForceLskf(forceLskf)
+            .build()
+        val cryptoObject = viewModel.getCryptoObject()
+        userAuthRequest.authenticate(cryptoObject)
     }
 
-    private fun sendResponseWithConsent(forceLskf : Boolean) {
-        val promptInfoBuilder = BiometricPrompt.PromptInfo.Builder()
-            .setTitle(getString(R.string.bio_auth_title))
-            .setSubtitle(getSubtitle())
-            .setDescription(formatEntryNames(vm.getEntryNames()))
-
-        if (forceLskf) {
-            // TODO: this works only on Android 11 or later but for now this is fine
-            //   as this is just a reference/test app and this path is only hit if
-            //   the user actually presses the "Use PIN" button.  Longer term, we should
-            //   fall back to using KeyGuard which will work on all Android versions.
-            promptInfoBuilder.setAllowedAuthenticators(
-                BiometricManager.Authenticators.DEVICE_CREDENTIAL);
-        } else {
-            if (BiometricManager.from(requireContext()).canAuthenticate(BIOMETRIC_STRONG)
-                == BIOMETRIC_SUCCESS
-            ) {
-                promptInfoBuilder.setNegativeButtonText(getString(R.string.bio_auth_use_pin))
-            } else {
-                // No biometrics enrolled, force use of LSKF
-                promptInfoBuilder.setDeviceCredentialAllowed(true)
-            }
-        }
-
-        val biometricPrompt = BiometricPrompt(this, executor, biometricAuthCallback)
-        val cryptoObject = vm.getCryptoObject()
-        val promptInfo = promptInfoBuilder.build()
-        if (cryptoObject != null) {
-            biometricPrompt.authenticate(promptInfo, cryptoObject)
-        } else {
-            biometricPrompt.authenticate(promptInfo)
-        }
+    private fun retryForcingPinUse() {
+        val runnable = { requestUserAuth(true) }
+        // Without this delay, the prompt won't reshow
+        Handler(Looper.getMainLooper()).postDelayed(runnable, 100)
     }
 
     private fun getSubtitle(): String {
@@ -212,35 +177,27 @@ class TransferDocumentFragment : Fragment() {
         } else {
             getString(R.string.bio_auth_verifier_anonymous)
         }
-
     }
 
     private fun formatEntryNames(documents: Map<Document, List<String>>): String {
-        val sb = StringBuffer()
+        val stringBuffer = StringBuffer()
         documents.forEach { (doc, entryNames) ->
-            sb.append("Document: ${doc.userVisibleName}\n")
+            stringBuffer.append("Document: ${doc.userVisibleName}\n")
             entryNames.forEach {
                 val stringId = resources.getIdentifier(it, "string", requireContext().packageName)
-                val entryName = if (stringId != 0) {
-                    getString(stringId)
-                } else {
-                    it
-                }
-                sb.append("• $entryName \n")
+                val entryName = if (stringId != 0) getString(stringId) else it
+                stringBuffer.append("• $entryName \n")
             }
-            sb.append("\n")
+            stringBuffer.append("\n")
         }
-
-        return sb.toString()
+        return stringBuffer.toString()
     }
 
     // Called when user consent is canceled or failed to provide biometric authentication
     private fun authenticationFailed() {
         runOnUiThread {
-            Toast.makeText(
-                requireContext(), getString(R.string.bio_auth_failed),
-                Toast.LENGTH_SHORT
-            ).show()
+            val message = getString(R.string.bio_auth_failed)
+            Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
         }
         onCloseConnection(
             sendSessionTerminationMessage = true,
@@ -252,7 +209,7 @@ class TransferDocumentFragment : Fragment() {
     private fun authenticationSucceeded() {
         // Send response again after user biometric authentication
         try {
-            if (vm.sendResponse() != null) {
+            if (viewModel.sendResponse() != null) {
                 // If this returns non-null it means that user-auth is still required.
                 //
                 // Wait, what? We just authenticated, so how can this happen?
@@ -263,60 +220,19 @@ class TransferDocumentFragment : Fragment() {
                 // if an ACP is configured with say a five second timeout this can happen if
                 // it takes more than five seconds for the user to press "Confirm"
                 //
-                Toast.makeText(
-                    requireContext(), "Auth took too long, try again",
-                    Toast.LENGTH_SHORT
-                ).show()
-                // Without this delay, the prompt won't reshow (bug in BiometricPrompt).
-                Handler().postDelayed({
-                    sendResponseWithConsent(false)
-                }, 100)
+                val message = "Auth took too long, try again"
+                Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+                retryForcingPinUse()
             }
         } catch (e: Exception) {
             val message = "Send response error: ${e.message}"
             Log.e(LOG_TAG, message, e)
-            Toast.makeText(
-                requireContext(), message,
-                Toast.LENGTH_SHORT
-            ).show()
+            Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
             binding.txtConnectionStatus.append("\n$message")
         }
     }
 
-    private val biometricAuthCallback = object : BiometricPrompt.AuthenticationCallback() {
-
-        override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-            super.onAuthenticationError(errorCode, errString)
-            // reached max attempts to authenticate the user, or authentication dialog was cancelled
-
-            if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON) {
-                // Without this delay, the prompt won't reshow
-                Handler().postDelayed({
-                    sendResponseWithConsent(true)
-                }, 100)
-            } else {
-                Log.d(
-                    LOG_TAG,
-                    "Attempt to authenticate the user has failed $errorCode - $errString"
-                )
-                authenticationFailed()
-            }
-        }
-
-        override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-            super.onAuthenticationSucceeded(result)
-            Log.d(LOG_TAG, "User authentication succeeded")
-            authenticationSucceeded()
-        }
-
-        override fun onAuthenticationFailed() {
-            super.onAuthenticationFailed()
-
-            Log.d(LOG_TAG, "Attempt to authenticate the user has failed")
-        }
-    }
-
-    var callback = object : OnBackPressedCallback(true /* enabled by default */) {
+    private var onBackPressedCallback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
             onDone()
         }
@@ -326,13 +242,16 @@ class TransferDocumentFragment : Fragment() {
         sendSessionTerminationMessage: Boolean,
         useTransportSpecificSessionTermination: Boolean
     ) {
-        vm.cancelPresentation(sendSessionTerminationMessage, useTransportSpecificSessionTermination)
+        viewModel.cancelPresentation(
+            sendSessionTerminationMessage,
+            useTransportSpecificSessionTermination
+        )
         hideButtons()
     }
 
     fun onDone() {
-        findNavController().navigate(
-            TransferDocumentFragmentDirections.actionTransferDocumentFragmentToSelectDocumentFragment()
-        )
+        val destination = TransferDocumentFragmentDirections
+            .actionTransferDocumentFragmentToSelectDocumentFragment()
+        findNavController().navigate(destination)
     }
 }
