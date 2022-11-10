55999379cb8f965e8840465f6eb91f6185db5ec3
David Zeuthen <zeuthen@google.com>
2022-11-10 09:46:11 -0500

    Add preliminary support for mdoc:// URIs (#137)
    This is used for 18013-7 presentations over the Internet
    
    Test: tested with Chrome on Android

--- appholder/src/main/java/com/android/mdl/app/MainActivity.kt
+++ appholder/src/main/java/com/android/mdl/app/MainActivity.kt
@@ -2,14 +2,23 @@ package com.android.mdl.app
 
 import android.app.PendingIntent
 import android.content.Intent
+import android.net.Uri
 import android.nfc.NfcAdapter
 import android.os.Bundle
 import android.util.Log
+import android.util.LogPrinter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.android.identity.OriginInfo
+import com.android.identity.OriginInfoWebsite
+import com.android.mdl.app.fragment.ReverseEngagementFragmentDirections
+import com.android.mdl.app.fragment.TransferDocumentFragment
+import com.android.mdl.app.viewmodel.ShareDocumentViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,10 +47,10 @@ class MainActivity : AppCompatActivity() {
         // Create a generic PendingIntent that will be deliver to this activity. The NFC stack
         // will fill in the intent with the details of the discovered tag before delivering to
         // this activity.
-        val intent = Intent(this, javaClass).apply {
+        val i = Intent(this, javaClass).apply {
             addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
         }
-        mPendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        mPendingIntent = PendingIntent.getActivity(this, 0, i, PendingIntent.FLAG_IMMUTABLE)
     }
 
     override fun onResume() {
@@ -57,6 +66,38 @@ class MainActivity : AppCompatActivity() {
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         Log.d(LOG_TAG, "New intent on Activity $intent")
+
+        if (intent == null) {
+            return;
+        }
+
+        var mdocUri : String? = null
+        var mdocReferrerUri : String? = null
+        if (intent.scheme.equals("mdoc")) {
+            val uri = Uri.parse(intent.toUri(0))
+            mdocUri = "mdoc://" + uri.authority
+
+            mdocReferrerUri = intent.extras?.get(Intent.EXTRA_REFERRER)?.toString()
+        }
+
+        if (mdocUri == null) {
+            Log.e(LOG_TAG, "No mdoc:// URI")
+            return
+        }
+        if (mdocReferrerUri == null) {
+            Log.e(LOG_TAG, "No referrer URI")
+            return
+        }
+
+        Log.i(LOG_TAG, "uri: $mdocUri")
+        Log.i(LOG_TAG, "referrer: $mdocReferrerUri")
+
+        val originInfos = ArrayList<OriginInfo>()
+        originInfos.add(OriginInfoWebsite(1, mdocReferrerUri))
+        val vm = ViewModelProvider(this).get(ShareDocumentViewModel::class.java)
+        vm.startPresentationReverseEngagement(mdocUri, originInfos)
+        val navController = findNavController(R.id.nav_host_fragment)
+        navController.navigate(R.id.transferDocumentFragment)
     }
 
     override fun onSupportNavigateUp(): Boolean {
