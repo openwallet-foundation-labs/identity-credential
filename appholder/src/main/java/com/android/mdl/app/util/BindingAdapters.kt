945f70ebfc04bc1b1c61cb94e43e120bbb95c958
Jov Mit <mitrejcevski@gmail.com>
2022-11-10 12:55:52 +0100

    Fix navigation crash

--- appholder/src/main/java/com/android/mdl/app/util/BindingAdapters.kt
+++ appholder/src/main/java/com/android/mdl/app/util/BindingAdapters.kt
@@ -1,8 +1,10 @@
 package com.android.mdl.app.util
 
 import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.databinding.BindingAdapter
+
 
 object BindingAdapters {
     /**
@@ -13,6 +15,7 @@ object BindingAdapters {
     @JvmStatic
     fun engagementView(view: LinearLayout, viewEngagement: View?) {
         viewEngagement?.let {
+            (viewEngagement.parent as? ViewGroup)?.removeView(viewEngagement)
             view.addView(it)
         }
     }
