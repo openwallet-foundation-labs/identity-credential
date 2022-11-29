12a96821c186a0033951e353f8692e32cfa36c1b
Jov Mit <mitrejcevski@gmail.com>
2022-11-29 22:29:45 +0100

    Apply Material3 theme with dynamic colors

--- appverifier/src/main/java/com/android/mdl/appreader/MainActivity.kt
+++ appverifier/src/main/java/com/android/mdl/appreader/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.elevation.SurfaceColors
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +24,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val color = SurfaceColors.SURFACE_2.getColor(this)
+        window.statusBarColor = color
+        window.navigationBarColor = color
         setContentView(R.layout.activity_main)
 
         val navController = findNavController(R.id.nav_host_fragment)
