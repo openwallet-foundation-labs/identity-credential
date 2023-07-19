64665bc0d38a2bbc7c48df2a027e75967ba60d1c
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2023-07-20 01:39:26 +0300

    Reader NFC tap enabled on landing page (#328)

--- appverifier/src/main/java/com/android/mdl/appreader/MainActivity.kt
+++ appverifier/src/main/java/com/android/mdl/appreader/MainActivity.kt
@@ -5,10 +5,15 @@ import android.content.Intent
 import android.nfc.NfcAdapter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.android.mdl.appreader.databinding.ActivityMainBinding
 import com.android.mdl.appreader.util.logDebug
 import com.google.android.material.elevation.SurfaceColors
 import java.security.Security
@@ -16,27 +21,22 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     private var mAdapter: NfcAdapter? = null
     private var mPendingIntent: PendingIntent? = null
+    private val navController by lazy {
+        Navigation.findNavController(this, R.id.nav_host_fragment)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val color = SurfaceColors.SURFACE_2.getColor(this)
         window.statusBarColor = color
         window.navigationBarColor = color
-        setContentView(R.layout.activity_main)
-
-        val navController = findNavController(R.id.nav_host_fragment)
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.RequestOptions,
-                R.id.SelectTransport,
-                R.id.ShowDocument,
-                R.id.Transfer
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupDrawerLayout()
 
         mAdapter = NfcAdapter.getDefaultAdapter(this)
         // Create a generic PendingIntent that will be deliver to this activity. The NFC stack
@@ -48,6 +48,11 @@ class MainActivity : AppCompatActivity() {
         mPendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
         Security.addProvider(BouncyCastleProvider())
+    }
+
+    private fun setupDrawerLayout() {
+        binding.nvSideDrawer.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.dlMainDrawer)
     }
 
     override fun onResume() {
@@ -66,8 +71,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        return NavigationUI.navigateUp(navController, binding.dlMainDrawer)
+    }
+
+    override fun onBackPressed() {
+        if (binding.dlMainDrawer.isDrawerOpen(GravityCompat.START)) {
+            binding.dlMainDrawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
