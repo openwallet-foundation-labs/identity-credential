9b0630f693eb3a6955cb587143087502ee01f483
Jov Mit <mitrejcevski@gmail.com>
2023-10-16 21:17:31 +0200

    Refresh Credential's Auth Keys Daily

--- appholder/src/main/java/com/android/identity/wallet/HolderApp.kt
+++ appholder/src/main/java/com/android/identity/wallet/HolderApp.kt
@@ -3,6 +3,7 @@ package com.android.identity.wallet
 import android.app.Application
 import com.android.identity.android.util.AndroidLogPrinter
 import com.android.identity.util.Logger
+import com.android.identity.wallet.util.PeriodicKeysRefreshWorkRequest
 import com.android.identity.wallet.util.PreferencesHelper
 import com.google.android.material.color.DynamicColors
 
@@ -13,5 +14,6 @@ class HolderApp: Application() {
         Logger.setLogPrinter(AndroidLogPrinter())
         DynamicColors.applyToActivitiesIfAvailable(this)
         PreferencesHelper.initialize(this)
+        PeriodicKeysRefreshWorkRequest(this).schedulePeriodicKeysRefreshing()
     }
 }
