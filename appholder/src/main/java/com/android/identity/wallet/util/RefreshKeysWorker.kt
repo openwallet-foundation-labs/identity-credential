a1b37a5143b15d66fa91903705edac8159ad4cb3
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2023-10-20 00:09:34 +0200

    Refresh Credential's Auth Keys Daily (#390)

---
+++ appholder/src/main/java/com/android/identity/wallet/util/RefreshKeysWorker.kt
@@ -0,0 +1,23 @@
+package com.android.identity.wallet.util
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.android.identity.wallet.document.DocumentManager
+
+class RefreshKeysWorker(
+    context: Context,
+    params: WorkerParameters
+) : Worker(context, params) {
+
+    private val documentManager = DocumentManager.getInstance(context)
+    private val provisioningUtil = ProvisioningUtil.getInstance(context)
+
+    override fun doWork(): Result {
+        documentManager.getDocuments().forEach { documentInformation ->
+            val credential = documentManager.getCredentialByName(documentInformation.docName)
+            credential?.let { provisioningUtil.refreshAuthKeys(it, documentInformation) }
+        }
+        return Result.success()
+    }
+}
