33c94a4b73540439c6712b73db896f11cf4e2ac8
Kees Geluk <80159@global.ul.com>
2023-12-12 19:42:05 +0100

    TrustManager in Identifity Library and Verifier App Signed-off-by: Kees Geluk <80159@global.ul.com>

--- appverifier/src/main/java/com/android/mdl/appreader/VerifierApp.kt
+++ appverifier/src/main/java/com/android/mdl/appreader/VerifierApp.kt
@@ -4,16 +4,31 @@ import android.app.Application
 import com.android.identity.android.util.AndroidLogPrinter
 import com.android.identity.util.Logger
 import androidx.preference.PreferenceManager
+import com.android.identity.storage.GenericStorageEngine
+import com.android.identity.storage.StorageEngine
+import com.android.identity.trustmanagement.TrustManager
 import com.android.mdl.appreader.settings.UserPreferences
+import com.android.mdl.appreader.util.KeysAndCertificates
 import com.google.android.material.color.DynamicColors
 import org.bouncycastle.jce.provider.BouncyCastleProvider
+import java.io.ByteArrayInputStream
 import java.security.Security
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
 
 class VerifierApp : Application() {
 
     private val userPreferences by lazy {
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
         UserPreferences(sharedPreferences)
+    }
+
+    private val trustManager by lazy {
+        TrustManager()
+    }
+
+    private val certificateStorageEngine by lazy {
+        GenericStorageEngine(getDir("Certificates", MODE_PRIVATE))
     }
 
     override fun onCreate() {
@@ -26,14 +41,33 @@ class VerifierApp : Application() {
         DynamicColors.applyToActivitiesIfAvailable(this)
         userPreferencesInstance = userPreferences
         Logger.setDebugEnabled(userPreferences.isDebugLoggingEnabled())
+        trustManagerInstance = trustManager
+        certificateStorageEngineInstance = certificateStorageEngine
+        certificateStorageEngineInstance.enumerate().forEach {
+            val certificate = parseCertificate(certificateStorageEngineInstance.get(it)!!)
+            trustManagerInstance.addCertificate(certificate)
+        }
+        KeysAndCertificates.getTrustedIssuerCertificates(this).forEach {
+            trustManagerInstance.addCertificate(it)
+        }
     }
 
     companion object {
 
         private lateinit var userPreferencesInstance: UserPreferences
+        lateinit var trustManagerInstance: TrustManager
+        lateinit var certificateStorageEngineInstance: StorageEngine
 
         fun isDebugLogEnabled(): Boolean {
             return userPreferencesInstance.isDebugLoggingEnabled()
         }
+    }
+
+    /**
+     * Parse a byte array an X509 certificate
+     */
+    private fun parseCertificate(certificateBytes: ByteArray): X509Certificate {
+        return CertificateFactory.getInstance("X509")
+            .generateCertificate(ByteArrayInputStream(certificateBytes)) as X509Certificate
     }
 }
