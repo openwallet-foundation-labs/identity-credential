4af0364e211402fcb3c0c0aa5bdeefeacd865d69
dritan-x <dritan.xhabija@gmail.com>
2024-02-28 17:34:00 -0800

    Addressing PR comments, removed navigation definition within WalletDestination and WalletNavigation, moved request processing business logic from PresentationActivity to TransferHelper
    Signed-off-by: dritan-x <dritan.xhabija@gmail.com>

--- wallet/src/main/java/com/android/identity_credential/wallet/util/CommonExtensions.kt
+++ wallet/src/main/java/com/android/identity_credential/wallet/util/CommonExtensions.kt
@@ -3,7 +3,6 @@ package com.android.identity_credential.wallet.util
 import android.content.Context
 import android.content.ContextWrapper
 import androidx.activity.ComponentActivity
-import com.android.identity.credentialtype.CredentialTypeRepository
 
 /**
  * Attempt to get a ComponentActivity or Activity from a context
@@ -13,15 +12,3 @@ fun Context.getActivity(): ComponentActivity? = when (this) {
     is ContextWrapper -> baseContext.getActivity()
     else -> null
 }
-
-/**
- * Extension function for extracting the display name for an element name in a CredentialRequest.DataElement
- */
-fun CredentialTypeRepository.stringValueFor(
-    docType: String,
-    namespace: String,
-    elementName: String
-) = getMdocCredentialType(docType)?.namespaces
-    ?.get(namespace)?.dataElements?.get(elementName)
-    ?.attribute?.displayName
-    ?: elementName
