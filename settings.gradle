eea9be6d2766bd0754b98006d059b3b982f341d0
David Zeuthen <zeuthen@google.com>
2024-03-15 15:28:29 -0400

    Check in a local copy of the credman SDK.
    This local copy will be deleted once the functionality is available in
    a released Android Credential Manager Jetpack.
    
    Test: Manually tested
    Signed-off-by: David Zeuthen <zeuthen@google.com>

--- settings.gradle
+++ settings.gradle
@@ -13,7 +13,7 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
