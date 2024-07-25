f989ce8b9fa97103df7f7ff655ed316ad0df486c
kdeus <kdeus@google.com>
2024-07-25 08:32:48 -0700

    Add selfie video support, along with ML Kit detection. (#684)
    This hooks up the UI flow, video recording, and evidence propagation to
    the view model and wallet server.
    
    This also adds ML Kit, hooks it into the Selfie camera recording code, detects
    all the current poses we support (and adds support for detecting smiling),
    and replaces the button to proceed to the next pose.
    
    Tested by:
    - Manual tests in the wallet app
    - ./gradlew check
    
    Fixes #642
    Fixes #680
    Signed-off-by: Kevin Deus <kdeus@google.com>

--- wallet/build.gradle.kts
+++ wallet/build.gradle.kts
@@ -91,6 +91,7 @@ dependencies {
     implementation(project(":mrtd-reader-android"))
     implementation(project(":jpeg2k"))
 
+    implementation(libs.kotlinx.coroutines.guava)
     implementation(libs.kotlinx.datetime)
     implementation(libs.kotlinx.io.core)
     implementation(libs.kotlinx.io.bytestring)
@@ -110,6 +111,7 @@ dependencies {
     implementation(compose.components.resources)
     implementation(compose.components.uiToolingPreview)
     implementation(compose.material)
+    implementation(libs.camera.lifecycle)
 
     debugImplementation(compose.uiTooling)
     implementation(compose.preview)
@@ -125,6 +127,7 @@ dependencies {
     implementation(libs.nimbus.oauth2.oidc.sdk)
     implementation(libs.androidx.camera.view)
     implementation(libs.androidx.material)
+    implementation(libs.face.detection)
     implementation(libs.zxing.core)
 
     implementation(files("../third-party/play-services-identity-credentials-0.0.1-eap01.aar"))
