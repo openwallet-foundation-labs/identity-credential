cc6ec18bf7a0d21032059d70125c109061aee2be
David Zeuthen <zeuthen@google.com>
2024-08-19 12:01:41 -0400

    Set up Maven publishing rules.
    Setting up to do a new release, this adds build system rules so we can
    release the following libraries
    
    - `identity`
    - `identity-mdoc`
    - `identity-doctypes`
    - `identity-android`
    - `identity-android-legacy`
    - `processor-annotations`
    
    This change also ports `identity-doctypes` to Kotlin Multiplatform.
    
    Test: ./gradlew check
    Test: ./gradlew connectedCheck
    Test: Manully tested with artifacts from "./gradlew publishToMavenLocal" in out-of-tree version of samples/simple-verifier.
    
    Signed-off-by: David Zeuthen <zeuthen@google.com>

--- identity-mdoc/build.gradle.kts
+++ identity-mdoc/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    id("maven-publish")
 }
+
+val projectVersionCode: Int by rootProject.extra
+val projectVersionName: String by rootProject.extra
 
 kotlin {
     jvmToolchain(17)
@@ -53,6 +57,27 @@ kotlin {
             dependencies {
                 implementation(libs.bouncy.castle.bcprov)
                 implementation(libs.bouncy.castle.bcpkix)
+            }
+        }
+    }
+}
+
+group = "com.android.identity"
+version = projectVersionName
+
+publishing {
+    repositories {
+        maven {
+            url = uri("${rootProject.rootDir}/repo")
+        }
+    }
+    publications.withType(MavenPublication::class) {
+        pom {
+            licenses {
+                license {
+                    name = "Apache 2.0"
+                    url = "https://opensource.org/licenses/Apache-2.0"
+                }
             }
         }
     }
