708e916a7a8cf22f43e2306cde5e1de523e3015f
oil-bleach-re-el-idited <oil-bleach-reprint@duck.com>
2024-12-22 02:13:50 -0500

    Delete appholder directory
    Signed-off-by: oil-bleach-re-el-idited <oil-bleach-reprint@duck.com>

--- appholder/src/main/java/com/android/identity/wallet/support/SecureAreaSupportState.kt
+++
@@ -1,8 +0,0 @@
-package com.android.identity.wallet.support
-
-import android.os.Parcelable
-import com.android.identity.wallet.composables.state.MdocAuthOption
-
-interface SecureAreaSupportState : Parcelable {
-    val mDocAuthOption: MdocAuthOption
-}
