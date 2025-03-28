908c61ff1bd9707c893ec2411f6cfc8fbce4eaee
Peter Sorotokin <sorotokin@gmail.com>
2025-03-28 07:39:00 -0700

    Coroutine usage cleanup. (#935)
    Fixed some of the more obvious problems: incorrect use of coroutine APIs, consuming `CancellationException`, launching in scopes that are not cancelled at appropriate time.
    
    Signed-off-by: Peter Sorotokin <sorotokin@gmail.com>

--- samples/testapp/src/androidMain/kotlin/com/android/identity/testapp/MainActivity.kt
+++ samples/testapp/src/androidMain/kotlin/com/android/identity/testapp/MainActivity.kt
@@ -7,9 +7,8 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.coroutineScope
 import org.multipaz.context.initializeApplication
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.multipaz.util.Logger
 
@@ -47,7 +46,7 @@ class MainActivity : FragmentActivity() {
         initializeApplication(this.applicationContext)
         enableEdgeToEdge()
 
-        CoroutineScope(Dispatchers.Main).launch {
+        lifecycle.coroutineScope.launch {
             val app = App.getInstance(NdefService.promptModel)
             app.startExportDocumentsToDigitalCredentials()
             setContent {
