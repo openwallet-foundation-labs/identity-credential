cede077195cd58ec374ea239cbbf48c90a6bad90
David Zeuthen <zeuthen@google.com>
2025-05-30 12:22:13 -0400

    Post-release version bump to 0.92.0. (#1017)
    Test: Compiles.
    
    Signed-off-by: David Zeuthen <zeuthen@google.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ val projectVersionCode: Int by extra {
 // following the release should bump it to the next version number.
 //
 val projectVersionLast = "0.91.0"
-val projectVersionNext = ""
+val projectVersionNext = "0.92.0"
 
 private fun runCommand(args: List<String>): String {
     val stdout = ByteArrayOutputStream()
