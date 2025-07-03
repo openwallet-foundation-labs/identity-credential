openwallet-foundation-labs/identity-credential

commit fe28cafdb8ab92790a19880b8c2764f88ec09c97

QR Code cleanups. (#1102)
For generation, move `generateQrCode` into `generateQrCode.kt` file in
the `qrcode` package. Also fix up the iOS implementation to generate a
quiet zone four modules wide.

For scanning, implement new `QrCodeScanner()` composable which on iOS
is using Vision Framework (by extending the existing iOS `Camera()`
composable) and on Android the scanner from ZXing (which we know works
because it's what we've been using in appverifier for a long time).

Also remove `ScanQrDialog()` and `ShowQrDialog()` from
multipaz-compose and just add them to where they're used right now
which are `samples/testapp` and `wallet` modules. The reason for this
is that these two composables are too opinionated to go into a general
purpose library.

This allows us to drop both qrose and easyqrscan dependencies which
makes supply chain management simpler for downstreams. It should also
help reduce APK sizes since we're no longer pulling in mlkit-barcodes
just for multipaz-compose users.

Also add `encodeImageToPng()` helper since this is needed for
notifications on iOS and it was using qrose for that.

Test: Manually tested on iOS and Android versions of testapp.

Signed-off-by: David Zeuthen <zeuthen@google.com>

multipaz-compose/src/androidMain/kotlin/org/multipaz/compose/qrcode/QrCodeScanner.android.kt
@@ -1,0 +1,64 @@
+package org.multipaz.compose.qrcode
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.google.zxing.BinaryBitmap
+import com.google.zxing.MultiFormatReader
+import com.google.zxing.PlanarYUVLuminanceSource
+import com.google.zxing.common.HybridBinarizer
+import org.multipaz.compose.camera.Camera
+import org.multipaz.compose.camera.CameraCaptureResolution
+import org.multipaz.compose.camera.CameraSelection
+
+@Composable
+actual fun QrCodeScanner(
+    modifier: Modifier,
+    cameraSelection: CameraSelection,
+    captureResolution: CameraCaptureResolution,
+    showCameraPreview: Boolean,
+    onCodeScanned: (qrCode: String?) -> Unit
+) {
+    var lastResult by remember { mutableStateOf<String?>(null) }
+    Camera(
+        modifier = modifier,
+        cameraSelection = cameraSelection,
+        captureResolution = captureResolution,
+        showCameraPreview = showCameraPreview,
+        onFrameCaptured = { cameraFrame ->
+            val imageProxy = cameraFrame.cameraImage.imageProxy
+            val yuvData = if (imageProxy.planes[0].buffer.hasArray()) {
+                imageProxy.planes[0].buffer.array()
+            } else {
+                val byteArray = ByteArray(imageProxy.planes[0].buffer.remaining())
+                imageProxy.planes[0].buffer.get(byteArray)
+                byteArray
+            }
+            val source = PlanarYUVLuminanceSource(
+                /* yuvData = */ yuvData,
+                /* dataWidth = */ imageProxy.planes[0].rowStride,
+                /* dataHeight = */ imageProxy.height,
+                /* left = */ 0,
+                /* top = */ 0,
+                /* width = */ imageProxy.width,
+                /* height = */ imageProxy.height,
+                /* reverseHorizontal = */ false
+            )
+            val bitmap = BinaryBitmap(HybridBinarizer(source));
+            try {
+                val result = MultiFormatReader().decode(bitmap)
+                onCodeScanned(result.text)
+                lastResult = result.text
+            } catch (_ : Throwable) {
+                // QR code not found in this frame
+                if (lastResult != null) {
+                    onCodeScanned(null)
+                }
+                lastResult = null
+            }
+        }
+    )
+}

multipaz-compose/src/commonMain/kotlin/org/multipaz/compose/qrcode/QrCodeScanner.kt
@@ -1,0 +1,31 @@
+package org.multipaz.compose.qrcode
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.multipaz.compose.camera.CameraCaptureResolution
+import org.multipaz.compose.camera.CameraSelection
+
+/**
+ * A composable for scanning QR codes.
+ *
+ * On iOS this is implemented using the platform Vision API and on Android it's using the
+ * [ZXing](https://github.com/zxing/zxing) library.
+ *
+ * This requires camera permission, see [org.multipaz.compose.permissions.rememberCameraPermissionState].
+ *
+ * @param modifier The composition modifier to apply to the composable.
+ * @param cameraSelection The desired hardware camera to use.
+ * @param captureResolution The desired resolution to use for the camera capture.
+ * @param showCameraPreview Whether to show the preview of the captured frame within the composable.
+ * @param onCodeScanned a callback which will be called when a QR code has been detected (in which
+ *   case the contents are passed) and when a QR code is no longer detected (in which case `null`
+ *   is passed). Will never be called with `null` multiple times in a row.
+ */
+@Composable
+expect fun QrCodeScanner(
+    modifier: Modifier = Modifier,
+    cameraSelection: CameraSelection,
+    captureResolution: CameraCaptureResolution,
+    showCameraPreview: Boolean,
+    onCodeScanned: (qrCode: String?) -> Unit
+)

multipaz-compose/src/iosMain/kotlin/org/multipaz/compose/camera/Camera.ios.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Matrix
 import androidx.compose.ui.viewinterop.UIKitInteropProperties
 import androidx.compose.ui.viewinterop.UIKitView
@@ -31,12 +32,15 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.yield
 import org.multipaz.util.Logger
 import platform.AVFoundation.AVCaptureConnection
 import platform.AVFoundation.AVCaptureDevice
 import platform.AVFoundation.AVCaptureDeviceInput
 import platform.AVFoundation.AVCaptureDevicePositionBack
 import platform.AVFoundation.AVCaptureDevicePositionFront
+import platform.AVFoundation.AVCaptureMetadataOutput
+import platform.AVFoundation.AVCaptureMetadataOutputObjectsDelegateProtocol
 import platform.AVFoundation.AVCaptureOutput
 import platform.AVFoundation.AVCaptureSession
 import platform.AVFoundation.AVCaptureSessionPreset1280x720
@@ -52,6 +56,7 @@ import platform.AVFoundation.AVCaptureVideoOrientationPortraitUpsideDown
 import platform.AVFoundation.AVCaptureVideoPreviewLayer
 import platform.AVFoundation.AVLayerVideoGravityResizeAspectFill
 import platform.AVFoundation.AVMediaTypeVideo
+import platform.AVFoundation.AVMetadataObjectTypeQRCode
 import platform.AVFoundation.position
 import platform.CoreGraphics.CGImageRef
 import platform.CoreGraphics.CGImageRelease
@@ -85,6 +90,7 @@ import platform.darwin.dispatch_queue_create
 import platform.darwin.dispatch_sync
 import kotlin.native.runtime.GC
 import kotlin.native.runtime.NativeRuntimeApi
+import platform.AVFoundation.AVMetadataMachineReadableCodeObject
 
 private const val TAG = "Camera"
 
@@ -96,12 +102,32 @@ actual fun Camera(
     showCameraPreview: Boolean,
     onFrameCaptured: suspend (frame: CameraFrame) -> Unit
 ) {
+    IosCamera(
+        modifier = modifier,
+        cameraSelection = cameraSelection,
+        captureResolution = captureResolution,
+        showCameraPreview = showCameraPreview,
+        onFrameCaptured = onFrameCaptured,
+        onQrCodeScanned = null
+    )
+}
+
+@Composable
+internal fun IosCamera(
+    modifier: Modifier,
+    cameraSelection: CameraSelection,
+    captureResolution: CameraCaptureResolution,
+    showCameraPreview: Boolean,
+    onFrameCaptured: (suspend (frame: CameraFrame) -> Unit)?,
+    onQrCodeScanned: ((qrCode: String?) -> Unit)?
+) {
     BoxWithConstraints(modifier) {
         val cameraManager = remember(cameraSelection, captureResolution) {
             CameraManager(
                 cameraSelection = cameraSelection,
                 captureResolution = captureResolution,
                 onCameraFrameCaptured = onFrameCaptured,
+                onQrCodeScanned = onQrCodeScanned,
                 isShowingPreview = showCameraPreview
             )
         }
@@ -137,7 +163,7 @@ actual fun Camera(
                 if (cameraManager.isPreviewLayerInitialized) {
                     isCameraReadyForPreview = true
                 } else {
-                    kotlinx.coroutines.yield() // Allow time for DisposableEffect.
+                    yield() // Allow time for DisposableEffect.
                     // Re-check after yield, DisposableEffect might have completed.
                     if (cameraManager.isPreviewLayerInitialized) {
                         isCameraReadyForPreview = true
@@ -198,9 +224,12 @@ private class CameraPreviewView(
 private class CameraManager(
     val cameraSelection: CameraSelection,
     val captureResolution: CameraCaptureResolution,
-    val onCameraFrameCaptured: suspend (cameraFrame: CameraFrame) -> Unit,
+    val onCameraFrameCaptured: (suspend (cameraFrame: CameraFrame) -> Unit)?,
+    val onQrCodeScanned: ((qrCode: String?) -> Unit)?,
     val isShowingPreview: Boolean
-) : AVCaptureVideoDataOutputSampleBufferDelegateProtocol, NSObject() {
+) : AVCaptureVideoDataOutputSampleBufferDelegateProtocol,
+    AVCaptureMetadataOutputObjectsDelegateProtocol,
+    NSObject() {
 
     lateinit var captureSession: AVCaptureSession
         private set
@@ -285,9 +314,29 @@ private class CameraManager(
         if (captureSession.canAddOutput(videoDataOutput)) {
             captureSession.addOutput(videoDataOutput)
         } else {
-            Logger.e(TAG, "Error adding output.")
+            Logger.e(TAG, "Error adding video data output")
             if (::captureSession.isInitialized && captureSession.isRunning()) captureSession.stopRunning()
             return
+        }
+
+
+        if (onQrCodeScanned != null) {
+            val metadataOutput = AVCaptureMetadataOutput()
+            if (captureSession.canAddOutput(metadataOutput)) {
+                captureSession.addOutput(metadataOutput)
+            } else {
+                Logger.e(TAG, "Error adding metadata output")
+                if (::captureSession.isInitialized && captureSession.isRunning()) captureSession.stopRunning()
+                return
+            }
+
+            metadataOutput.metadataObjectTypes = listOf(
+                AVMetadataObjectTypeQRCode
+            )
+            metadataOutput.setMetadataObjectsDelegate(
+                objectsDelegate = this,
+                queue = dispatch_get_main_queue()
+            )
         }
 
         withContext(Dispatchers.IO) {
@@ -358,7 +407,7 @@ private class CameraManager(
                 return
             }
 
-            val boundsToUse = currentPreviewFrame!!
+            val boundsToUse = currentPreviewFrame
 
             previewTransformation = calculatePreviewTransformationMatrix(
                 imageWidth = imageWidth,
@@ -378,21 +427,43 @@ private class CameraManager(
             )
         }
 
-        val cameraFrame = CameraFrame(
-            cameraImage = cameraImage,
-            width = imageWidth.toInt(),
-            height = imageHeight.toInt(),
-            rotation = videoOrientation.toRotationAngle(),
-            previewTransformation = previewTransformation,
-        )
+        if (onCameraFrameCaptured != null) {
+            val cameraFrame = CameraFrame(
+                cameraImage = cameraImage,
+                width = imageWidth.toInt(),
+                height = imageHeight.toInt(),
+                rotation = videoOrientation.toRotationAngle(),
+                previewTransformation = previewTransformation,
+            )
 
-        runBlocking {
-            onCameraFrameCaptured(cameraFrame)
+            runBlocking {
+                onCameraFrameCaptured(cameraFrame)
+            }
         }
 
         // Not recommended but IS needed to avoid lockups, see https://youtrack.jetbrains.com/issue/KT-74239
         GC.collect()
     }
+
+    override fun captureOutput(
+        output: AVCaptureOutput,
+        didOutputMetadataObjects: List<*>,
+        fromConnection: AVCaptureConnection
+    ) {
+        if (onQrCodeScanned == null) {
+            return
+        }
+        if (didOutputMetadataObjects.isEmpty()) {
+            onQrCodeScanned(null)
+        } else {
+            val metadataObj = didOutputMetadataObjects[0] as AVMetadataMachineReadableCodeObject
+            if (metadataObj.type == AVMetadataObjectTypeQRCode) {
+                onQrCodeScanned(metadataObj.stringValue)
+            }
+
+        }
+    }
+
 }
 
 private fun  AVCaptureVideoOrientation.toRotationAngle(): Int =

gradle/libs.versions.toml
@@ -57,8 +57,6 @@ jetbrains-navigation = "2.8.0-alpha10"
 jetbrains-lifecycle = "2.8.2"
 cameraLifecycle = "1.3.4"
 buildconfig = "5.3.5"
-qrose = "1.0.1"
-easyqrscan = "0.2.0"
 androidx-sqlite = "2.5.0-alpha12"
 play-services-identity-credentials = "16.0.0-alpha05"
 androidx-credentials = "1.5.0-rc01"
@@ -141,8 +139,6 @@ jetbrains-navigation-compose = { module = "org.jetbrains.androidx.navigation:nav
 jetbrains-navigation-runtime = { module = "org.jetbrains.androidx.navigation:navigation-runtime", version.ref="jetbrains-navigation" }
 jetbrains-lifecycle-viewmodel-compose = { module = "org.jetbrains.androidx.lifecycle:lifecycle-viewmodel-compose", version.ref="jetbrains-lifecycle" }
 camera-lifecycle = { group = "androidx.camera", name = "camera-lifecycle", version.ref = "cameraLifecycle" }
-qrose = { group = "io.github.alexzhirkevich", name = "qrose", version.ref="qrose"}
-easyqrscan = { module = "io.github.kalinjul.easyqrscan:scanner", version.ref = "easyqrscan" }
 androidx-sqlite = { module="androidx.sqlite:sqlite", version.ref = "androidx-sqlite" }
 androidx-sqlite-framework = { module="androidx.sqlite:sqlite-framework", version.ref = "androidx-sqlite" }
 androidx-sqlite-bundled = { module="androidx.sqlite:sqlite-bundled", version.ref = "androidx-sqlite" }

multipaz-compose/build.gradle.kts
@@ -75,8 +75,6 @@ kotlin {
                 implementation(project(":multipaz-models"))
                 implementation(libs.kotlinx.datetime)
                 implementation(libs.kotlinx.serialization.json)
-                implementation(libs.qrose)
-                implementation(libs.easyqrscan)
                 implementation(libs.kotlinx.io.core)
             }
         }

multipaz-compose/src/androidMain/kotlin/org/multipaz/compose/Util.android.kt
@@ -3,48 +3,25 @@ package org.multipaz.compose
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.core.graphics.createBitmap
-import com.google.zxing.BarcodeFormat
-import com.google.zxing.MultiFormatWriter
-import com.google.zxing.WriterException
-import com.google.zxing.common.BitMatrix
+import kotlinx.io.bytestring.ByteString
 import org.multipaz.compose.camera.CameraFrame
-import com.google.zxing.qrcode.QRCodeWriter
+import java.io.ByteArrayOutputStream
 
 actual fun decodeImage(encodedData: ByteArray): ImageBitmap {
     return BitmapFactory.decodeByteArray(encodedData, 0, encodedData.size).asImageBitmap()
 }
 
-actual fun generateQrCode(
-    url: String,
-): ImageBitmap {
-    val width = 800
-    val result: BitMatrix = try {
-        MultiFormatWriter().encode(
-            url,
-            BarcodeFormat.QR_CODE, width, width, null
-        )
-    } catch (e: WriterException) {
-        throw java.lang.IllegalArgumentException(e)
-    }
-    val w = result.width
-    val h = result.height
-    val pixels = IntArray(w * h)
-    for (y in 0 until h) {
-        val offset = y * w
-        for (x in 0 until w) {
-            pixels[offset + x] = if (result[x, y]) Color.BLACK else Color.WHITE
-        }
-    }
-    val bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
-    bitmap.setPixels(pixels, 0, width, 0, 0, w, h)
-    return bitmap.asImageBitmap()
+actual fun encodeImageToPng(image: ImageBitmap): ByteString {
+    val bitmap = image.asAndroidBitmap()
+    val baos = ByteArrayOutputStream()
+    bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos)
+    return ByteString(baos.toByteArray())
 }
-
 
 private val paint = Paint().apply {
     isAntiAlias = false

multipaz-compose/src/androidMain/kotlin/org/multipaz/compose/qrcode/generateQrCode.android.kt
@@ -1,0 +1,36 @@
+package org.multipaz.compose.qrcode
+
+import android.graphics.Bitmap
+import android.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.google.zxing.common.BitMatrix
+
+actual fun generateQrCode(
+    url: String,
+): ImageBitmap {
+    val width = 800
+    val result: BitMatrix = try {
+        MultiFormatWriter().encode(
+            url,
+            BarcodeFormat.QR_CODE, width, width, null
+        )
+    } catch (e: WriterException) {
+        throw java.lang.IllegalArgumentException(e)
+    }
+    val w = result.width
+    val h = result.height
+    val pixels = IntArray(w * h)
+    for (y in 0 until h) {
+        val offset = y * w
+        for (x in 0 until w) {
+            pixels[offset + x] = if (result[x, y]) Color.BLACK else Color.WHITE
+        }
+    }
+    val bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+    bitmap.setPixels(pixels, 0, width, 0, 0, w, h)
+    return bitmap.asImageBitmap()
+}

multipaz-compose/src/commonMain/composeResources/values/strings.xml
@@ -20,9 +20,6 @@
     <string name="consent_modal_bottom_sheet_data_element_icon_description">Data Element Icon</string>
     <string name="consent_modal_bottom_sheet_warning_icon_description">Warning Icon</string>
 
-    <!-- ShowQrCodeDialog -->
-    <string name="show_qr_code_dialog_qr_content_description">QR code image</string>
-
     <!-- CertificateViewer -->
     <string name="certificate_viewer_sub_basic_info">Basic Information</string>
     <string name="certificate_viewer_k_type">Type</string>

commit 588d94ff782ad3773f19ed69a970db6bd4bb159c

QrCodeScanner: Fix problem with onCodeScanned() being called multiple times.
This problem manifested itself in the _ISO mdoc Proximity Reading_
screen insofar that `doReaderFlow()` occasionally would be invoked
multiple times leading to the wrong SessionTranscript to be used
because a new ephemeral reader key was generated. This resulted in a
"Device Authentication failed" warning when showing the result.

This problem was introduced in Commit fe28caf where we changed the QR
code scanning library on Android to be ZXing.

Test: Manually tested and observed "Device Authentication failed" no longer is shown.
Signed-off-by: David Zeuthen <zeuthen@google.com>

multipaz-compose/src/androidMain/kotlin/org/multipaz/compose/qrcode/QrCodeScanner.android.kt
@@ -22,7 +22,7 @@ actual fun QrCodeScanner(
     showCameraPreview: Boolean,
     onCodeScanned: (qrCode: String?) -> Unit
 ) {
-    var lastResult by remember { mutableStateOf<String?>(null) }
+    var lastCodeScanned by remember { mutableStateOf<String?>(null) }
     Camera(
         modifier = modifier,
         cameraSelection = cameraSelection,
@@ -50,14 +50,16 @@ actual fun QrCodeScanner(
             val bitmap = BinaryBitmap(HybridBinarizer(source));
             try {
                 val result = MultiFormatReader().decode(bitmap)
-                onCodeScanned(result.text)
-                lastResult = result.text
+                if (result.text != lastCodeScanned) {
+                    onCodeScanned(result.text)
+                    lastCodeScanned = result.text
+                }
             } catch (_ : Throwable) {
                 // QR code not found in this frame
-                if (lastResult != null) {
+                if (lastCodeScanned != null) {
                     onCodeScanned(null)
+                    lastCodeScanned = null
                 }
-                lastResult = null
             }
         }
     )

multipaz-compose/src/commonMain/kotlin/org/multipaz/compose/qrcode/QrCodeScanner.kt
@@ -11,6 +11,11 @@ import org.multipaz.compose.camera.CameraSelection
  * On iOS this is implemented using the platform Vision API and on Android it's using the
  * [ZXing](https://github.com/zxing/zxing) library.
  *
+ * This provides the guarantee that [onCodeScanned] will never be called with `null` multiple
+ * times in a row and will never be called with the same QR Code multiple times in a row. However
+ * do note that in some situations the underlying QR code scanner might flicker so [onCodeScanned]
+ * will be called with the QR code, then `null`, the QR code, then `null`, many times in a row.
+ *
  * This requires camera permission, see [org.multipaz.compose.permissions.rememberCameraPermissionState].
  *
  * @param modifier The composition modifier to apply to the composable.
@@ -19,7 +24,7 @@ import org.multipaz.compose.camera.CameraSelection
  * @param showCameraPreview Whether to show the preview of the captured frame within the composable.
  * @param onCodeScanned a callback which will be called when a QR code has been detected (in which
  *   case the contents are passed) and when a QR code is no longer detected (in which case `null`
- *   is passed). Will never be called with `null` multiple times in a row.
+ *   is passed).
  */
 @Composable
 expect fun QrCodeScanner(

multipaz-compose/src/iosMain/kotlin/org/multipaz/compose/camera/Camera.ios.kt
@@ -445,6 +445,7 @@ private class CameraManager(
         GC.collect()
     }
 
+    var lastCodeScanned: String? = null
     override fun captureOutput(
         output: AVCaptureOutput,
         didOutputMetadataObjects: List<*>,
@@ -454,11 +455,17 @@ private class CameraManager(
             return
         }
         if (didOutputMetadataObjects.isEmpty()) {
-            onQrCodeScanned?.let { it(null) }
+            if (lastCodeScanned != null) {
+                onQrCodeScanned(null)
+                lastCodeScanned = null
+            }
         } else {
             val metadataObj = didOutputMetadataObjects[0] as AVMetadataMachineReadableCodeObject
             if (metadataObj.type == AVMetadataObjectTypeQRCode) {
-                onQrCodeScanned?.let { it(metadataObj.stringValue) }
+                if (lastCodeScanned != metadataObj.stringValue) {
+                    onQrCodeScanned(metadataObj.stringValue)
+                    lastCodeScanned = metadataObj.stringValue
+                }
             }
 
         }