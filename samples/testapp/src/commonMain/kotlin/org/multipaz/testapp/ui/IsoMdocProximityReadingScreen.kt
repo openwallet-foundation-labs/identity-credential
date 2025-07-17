9d058de830099bb79799e8f4b761a9ce27fe08c4
Vishnu Sanal T <vishnu@openmobilehub.org>
2025-07-17 20:33:34 +0530

    feat: implement `rememberBluetoothEnabledState` on the testapp
    Signed-off-by: Vishnu Sanal T <vishnu@openmobilehub.org>

--- samples/testapp/src/commonMain/kotlin/org/multipaz/testapp/ui/IsoMdocProximityReadingScreen.kt
+++ samples/testapp/src/commonMain/kotlin/org/multipaz/testapp/ui/IsoMdocProximityReadingScreen.kt
@@ -89,6 +89,7 @@ import kotlinx.io.bytestring.ByteString
 import org.multipaz.compose.cards.InfoCard
 import org.multipaz.compose.cards.WarningCard
 import org.multipaz.compose.decodeImage
+import org.multipaz.compose.permissions.rememberBluetoothEnabledState
 import org.multipaz.compose.permissions.rememberBluetoothPermissionState
 import org.multipaz.testapp.ui.ScanQrCodeDialog
 import org.multipaz.mdoc.role.MdocRole
@@ -139,6 +140,7 @@ fun IsoMdocProximityReadingScreen(
     val dropdownExpanded = remember { mutableStateOf(false) }
     val selectedRequest = remember { mutableStateOf(availableRequests[0]) }
     val blePermissionState = rememberBluetoothPermissionState()
+    val bleEnabledState = rememberBluetoothEnabledState()
     val coroutineScope = rememberCoroutineScope { app.promptModel }
     val readerShowQrScanner = remember { mutableStateOf(false) }
     val readerTransport = remember { mutableStateOf<MdocTransport?>(null) }
@@ -284,6 +286,22 @@ fun IsoMdocProximityReadingScreen(
                 }
             ) {
                 Text("Request BLE permissions")
+            }
+        }
+    }  else if (!bleEnabledState.isEnabled) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        bleEnabledState.enable()
+                    }
+                }
+            ) {
+                Text("Enable Bluetooth")
             }
         }
     } else {
